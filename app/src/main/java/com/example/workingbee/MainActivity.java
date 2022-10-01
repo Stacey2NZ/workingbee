e6ef7c650483655d7ceb7d5371986117c66f8636
Stacey <staceywatson1987@gmail.com>
2022-10-01 21:42:13 +1300

    Login Page designed

---
+++ app/src/main/java/com/example/workingbee/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.workingbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private final String LOGINTAG ="loginScreenTag";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button loginBtn = findViewById(R.id.loginBtnLogin);
+        final Button exitBtn = findViewById(R.id.exitBtnLogin);
+
+        // Code for login functions of checking if a user is selected, retrieving their data for next activity
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        // Code for Exit button to close app when clicked
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/workingbee/TaskInfoInput.java
@@ -0,0 +1,14 @@
+package com.example.workingbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskInfoInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_info_input);
+    }
+}
